deeefc3429ab81fc90c7e54435392d078f0c3da2
JeffHC0911 <jefferhc0911@gmail.com>
2024-07-06 23:50:35 -0500

    Render grades in table is correct. Fix bug when adding notes

--- src/teacher/components/TableComponent.jsx
+++ src/teacher/components/TableComponent.jsx
@@ -2,7 +2,6 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 
 "use client";
-//import Image from 'next/image'
 import { useEffect, useState } from "react";
 import { Badge, Button, Popover, Table } from "keep-react";
 import {
@@ -37,67 +36,59 @@ export const TableComponent = ({ courseId }) => {
     startLoadingGrade();
   }, []);
 
-  const filterAssesments = assesments.filter(
-    (assesment) => assesment.course._id === courseId
+  // Filtrar cursos por el ID del curso proporcionado
+  const course = courses.find((course) => course._id === courseId);
+  const courseAssessments = assesments.filter(
+    (assessment) => assessment.course._id === courseId
   );
 
-  /**
-   * Filtra las evaluaciones que coinciden con el curso del profesor
-   */
-
-  // const assesmentCourses = filterAssesments
-  // .map(assesment => ({
-  //   _id: assesment._id,
-  //   name: assesment.name,
-  //   weighted: assesment.weighted,
-  //   }))
-
-  const combinedData = courses
-    .filter((course) => course._id === courseId)
-    .map((course) => ({
-      students: course.students.map((student) => ({
-        id: student._id,
-        name: student.name,
-        lastname: student.lastname,
+  const combinedData = {
+    students: course
+      ? course.students.map((student) => ({
+          id: student._id.toString(),
+          name: student.name,
+          lastname: student.lastname,
+          status: student.status,
+        }))
+      : [],
+    assessments: courseAssessments.map((assessment) => ({
+      _id: assessment._id.toString(),
+      name: assessment.name,
+      weighted: assessment.weighted,
+      grades: assessment.grades.map((grade) => ({
+        _id: grade._id,
+        value: grade.value,
+        student: grade.student._id
       })),
-      assessments: assesments
-        .filter((assessment) => course._id === assessment.course._id)
-        .map((assessment) => ({
-          _id: assessment._id,
-          name: assessment.name,
-          weighted: assessment.weighted,
-          grades: assessment.grades
-            .filter((grade) => grades.some((g) => g._id === grade._id))
-            .map((grade) => ({
-              _id: grade._id,
-              value: grade.value,
-            })),
-        })),
-    }));
+    })),
+  };
+  
+  //console.log("Combined Data:", combinedData);
 
-  const calculateFinalGrade = (student, course) => {
+
+  const calculateFinalGrade = (student, courseData) => {
     let finalGrade = 0;
     let totalWeighted = 0;
-    let hasGrades = false; // Variable para verificar si el estudiante tiene notas en alguna evaluación
+    let hasGrades = false;
 
-    course.assessments.forEach((assessment) => {
+    courseData.assessments.forEach((assessment) => {
       const studentGrade = assessment.grades.find(
-        (grade) => grade.student === student._id
+        (grade) => grade.student === student.id
       );
 
       if (studentGrade) {
         finalGrade += studentGrade.value * assessment.weighted;
         totalWeighted += assessment.weighted;
-        hasGrades = true; // El estudiante tiene notas en alguna evaluación
+        hasGrades = true;
       }
     });
 
     if (!hasGrades) {
-      return "Sin notas"; // Retorna un mensaje indicando que el estudiante no tiene notas
+      return "Sin notas";
     }
 
     if (totalWeighted === 0) {
-      return NaN; // Retorna NaN si no hay evaluaciones con calificaciones
+      return NaN;
     }
 
     return finalGrade / totalWeighted;
@@ -147,116 +138,118 @@ export const TableComponent = ({ courseId }) => {
             <p className="text-body-6 font-medium text-metal-400">Name</p>
           </Table.HeadCell>
           <Table.HeadCell>Status</Table.HeadCell>
-          {filterAssesments.map((column) => (
-            <Table.HeadCell className="min-w-[152px]" key={column._id}>
-              {column.name} - {column.weighted} %
+          {combinedData.assessments.map((assessment) => (
+            <Table.HeadCell className="min-w-[152px]" key={assessment._id}>
+              {assessment.name} - {assessment.weighted} %
             </Table.HeadCell>
           ))}
           <Table.HeadCell>Definitive Grade</Table.HeadCell>
           <Table.HeadCell className="min-w-[100px]" />
         </Table.Head>
         <Table.Body className="divide-gray-25 divide-y">
-          {combinedData.map((course) =>
-            course.students.map((student) => (
-              <Table.Row key={student.id} className="bg-white">
-                <Table.Cell>
-                  <div className="flex items-center gap-3">
-                    <div className="flex items-center gap-4">
-                      <div className="flex items-center gap-2">
-                        <div>
-                          <p className="-mb-0.5 text-body-4 font-medium text-metal-600">
-                            {student.name}
-                          </p>
-                          <span>{student.lastname}</span>
-                        </div>
+          {combinedData.students.map((student) => (
+            <Table.Row key={student.id} className="bg-white">
+              <Table.Cell>
+                <div className="flex items-center gap-3">
+                  <div className="flex items-center gap-4">
+                    <div className="flex items-center gap-2">
+                      <div>
+                        <p className="-mb-0.5 text-body-4 font-medium text-metal-600">
+                          {student.name}
+                        </p>
+                        <span>{student.lastname}</span>
                       </div>
                     </div>
                   </div>
-                </Table.Cell>
-                <Table.Cell>
-                  <Badge
-                    colorType="light"
-                    color={student.status === "Active" ? "success" : "warning"}
-                    dot={true}
-                  >
-                    {student.status}
-                  </Badge>
-                </Table.Cell>
-                {course.assessments.map((assessment, index) => (
-                  <Table.Cell key={index}>
-                    {assessment.grades[index] && assessment.grades[index].value}
+                </div>
+              </Table.Cell>
+              <Table.Cell>
+                <Badge
+                  colorType="light"
+                  color={student.status === "Active" ? "success" : "warning"}
+                  dot={true}
+                >
+                  {student.status}
+                </Badge>
+              </Table.Cell>
+              {combinedData.assessments.map((assessment) => {
+                const studentGrade = assessment.grades.find(
+                  (grade) => grade.student === student.id
+                );
+                return (
+                  <Table.Cell key={assessment._id}>
+                    {studentGrade ? studentGrade.value : "N/A"}
                   </Table.Cell>
-                ))}
-                <Table.Cell>
-                  <div className="flex items-center gap-3">
-                    <div></div>
-                    <div className="flex items-center gap-1">
-                      <span>
-                        {student.finalGrade < 3 ? (
-                          <ArrowDown size={20} color="#FF0000" />
-                        ) : (
-                          <Check size={20} color="#008000" />
-                        )}
-                      </span>
-                      <span>{calculateFinalGrade(student, course)}</span>
-                    </div>
+                );
+              })}
+              <Table.Cell>
+                <div className="flex items-center gap-3">
+                  <div></div>
+                  <div className="flex items-center gap-1">
+                    <span>
+                      {calculateFinalGrade(student, combinedData) < 3 ? (
+                        <ArrowDown size={20} color="#FF0000" />
+                      ) : (
+                        <Check size={20} color="#008000" />
+                      )}
+                    </span>
+                    <span>{calculateFinalGrade(student, combinedData)}</span>
                   </div>
-                </Table.Cell>
-                <Table.Cell>
-                  <Popover
-                    showDismissIcon={false}
-                    showArrow={false}
-                    className="w-48 border border-metal-100 p-2"
-                  >
-                    <Popover.Container className="!mt-0 !block">
-                      <ul>
-                        <li className="rounded px-2 py-1 hover:bg-metal-100">
-                          <button className="flex w-full items-center justify-between text-body-4 font-normal text-metal-600">
-                            <span>Delete</span>
-                            <span>
-                              <Trash />
-                            </span>
-                          </button>
-                        </li>
-                        <li className="rounded px-2 py-1 hover:bg-metal-100">
-                          <button className="flex w-full items-center justify-between text-body-4 font-normal text-metal-600">
-                            <span>Edit</span>
-                            <span>
-                              <Pencil />
-                            </span>
-                          </button>
-                        </li>
-                        <li className="rounded px-2 py-1 hover:bg-metal-100">
-                          <button
-                            className="flex w-full items-center justify-between text-body-4 font-normal text-metal-600"
-                            onClick={() => handleAddGrades(student.id)}
-                          >
-                            <span>Add Grades</span>
-                            <span>
-                              <Pencil />
-                            </span>
-                          </button>
-                        </li>
-                      </ul>
-                    </Popover.Container>
-                    <Popover.Action>
-                      <Button type="outlineGray" size="xs" circle={true}>
-                        <DotsThreeOutline
-                          size={14}
-                          color="#5E718D"
-                          weight="bold"
-                        />
-                      </Button>
-                    </Popover.Action>
-                  </Popover>
-                </Table.Cell>
-              </Table.Row>
-            ))
-          )}
+                </div>
+              </Table.Cell>
+              <Table.Cell>
+                <Popover
+                  showDismissIcon={false}
+                  showArrow={false}
+                  className="w-48 border border-metal-100 p-2"
+                >
+                  <Popover.Container className="!mt-0 !block">
+                    <ul>
+                      <li className="rounded px-2 py-1 hover:bg-metal-100">
+                        <button className="flex w-full items-center justify-between text-body-4 font-normal text-metal-600">
+                          <span>Delete</span>
+                          <span>
+                            <Trash />
+                          </span>
+                        </button>
+                      </li>
+                      <li className="rounded px-2 py-1 hover:bg-metal-100">
+                        <button className="flex w-full items-center justify-between text-body-4 font-normal text-metal-600">
+                          <span>Edit</span>
+                          <span>
+                            <Pencil />
+                          </span>
+                        </button>
+                      </li>
+                      <li className="rounded px-2 py-1 hover:bg-metal-100">
+                        <button
+                          className="flex w-full items-center justify-between text-body-4 font-normal text-metal-600"
+                          onClick={() => handleAddGrades(student.id)}
+                        >
+                          <span>Add Grades</span>
+                          <span>
+                            <Pencil />
+                          </span>
+                        </button>
+                      </li>
+                    </ul>
+                  </Popover.Container>
+                  <Popover.Action>
+                    <Button type="outlineGray" size="xs" circle={true}>
+                      <DotsThreeOutline
+                        size={14}
+                        color="#5E718D"
+                        weight="bold"
+                      />
+                    </Button>
+                  </Popover.Action>
+                </Popover>
+              </Table.Cell>
+            </Table.Row>
+          ))}
         </Table.Body>
       </Table>
 
-      {/* Renderiza el componente AddGradesComponent si hay un estudiante seleccionado */}
       {selectedStudentId && (
         <AddGradesComponent studentId={selectedStudentId} courseId={courseId} />
       )}
